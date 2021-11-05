45647f4b1465fe2963700cb6a624fce984f79bee
Daniel Becker <danielbeckeralves@gmail.com>
2021-11-05 14:54:58 -0300

    adding cancel to clear errors on sign up

--- src/components/SignupModal.js
+++ src/components/SignupModal.js
@@ -108,6 +108,7 @@ export default function SignupModal(props) {
     setSenha({ ...senha, segundaSenha: "" });
     setEmail("");
     setContaCriada(false);
+    setContaErro("hidden");
   };
 
   return (
