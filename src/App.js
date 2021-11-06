72ba2b71678bb6266e8f7c1570b403e2d7a9ba20
Renan Silveira <renansilveira@Renans-MacBook-Pro-2.local>
2021-11-06 18:29:10 -0300

    add switch to a new route when page is not found

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 import "./App.css";
 import { Route, BrowserRouter as Router } from "react-router-dom";
+import {Switch} from 'react-router'
+
 
 function App() {
   return (
     <Router>
       <div>
+        <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/home" component={Home} />
+        <Route component={NotFound}/>
+        </Switch>
       </div>
     </Router>
   );
---
+++ src/components/NotFound.js
@@ -0,0 +1,8 @@
+import React from "react";
+import NotFoundImage from "../assets/error.jpg";
+import "./NotFound.css";
+
+const NotFound = () =>
+ <img className="notfound" src={NotFoundImage}></img>
+
+export default NotFound
